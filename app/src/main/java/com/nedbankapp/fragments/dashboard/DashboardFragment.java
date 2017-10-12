7c9148e6a08b59c0d11fab684ad92f2ebae963b9
kr28mevada <kr28mevada@gmail.com>
2017-10-12 14:53:18 +0530

    Latest code commited 6

--- app/src/main/java/com/nedbankapp/fragments/dashboard/DashboardFragment.java
+++ app/src/main/java/com/nedbankapp/fragments/dashboard/DashboardFragment.java
@@ -3,13 +3,18 @@ package com.nedbankapp.fragments.dashboard;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nedbankapp.R;
 import com.nedbankapp.fragments.common.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,7 +34,11 @@ public class DashboardFragment extends BaseFragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    @BindView(R.id.rv_horizontal)
+    RecyclerView rv_horizontal;
+
+    @BindView(R.id.rv_vertical)
+    RecyclerView rv_vertical;
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -70,42 +79,12 @@ public class DashboardFragment extends BaseFragment {
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this,view);
+
+
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
