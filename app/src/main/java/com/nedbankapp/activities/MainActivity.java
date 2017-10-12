0954717309ab19f21a77df54650f7004ddc0405a
kr28mevada <kr28mevada@gmail.com>
2017-10-12 11:57:46 +0530

    Latest code commited 4

--- app/src/main/java/com/nedbankapp/activities/MainActivity.java
+++ app/src/main/java/com/nedbankapp/activities/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends BaseActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
         //TEST
         //demo
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
